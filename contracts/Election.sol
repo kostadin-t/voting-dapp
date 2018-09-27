138c1e6a98bea28f4fcba12bec34c40b5d49292c
Kostadin Terziev <kocyo.terziev@gmail.com>
2018-09-27 21:02:10 +0300

    Tests and front end

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,39 @@ pragma solidity ^0.4.2;
 
 contract Election {
 
-	string public candidate;
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+
+	}
+
+
+	mapping(address => bool) public voters;
+
+	mapping(uint => Candidate) public candidates;
+
+	uint public candidatesCount;
 
 	constructor () public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string _name) {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote (uint _candidateId) public {
+
+		require(!voters[msg.sender]);
+
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+		
+		voters[msg.sender] = true;
+
+		candidates[_candidateId].voteCount ++;
+
 	}
 }
