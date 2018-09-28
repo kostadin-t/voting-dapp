146aa4672a6eab68db10c35d6c48d6d2a2548b1c
Kostadin Terziev <kocyo.terziev@gmail.com>
2018-09-28 12:13:33 +0300

    Final changes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -16,6 +16,10 @@ contract Election {
 
 	uint public candidatesCount;
 
+	event votedEvent (
+		uint indexed_candidateId
+	);
+
 	constructor () public {
 		addCandidate("Candidate 1");
 		addCandidate("Candidate 2");
@@ -35,6 +39,8 @@ contract Election {
 		voters[msg.sender] = true;
 
 		candidates[_candidateId].voteCount ++;
+
+		votedEvent(_candidateId);
 
 	}
 }
